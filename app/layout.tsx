44710b5634e8cdcb8797232347a602aba52ee71b
Rafael Leitao <carlosrafael093@gmail.com>
2024-10-14 00:16:55 +0200

    feat(app): Modify to show GroupPage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
+// import DeployButton from "@/src/components/deploy-button";
+import { EnvVarWarning } from "@/src/components/env-var-warning";
+import HeaderAuth from "@/src/components/header-auth";
+import { ThemeSwitcher } from "@/src/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
-import "./globals.css";
+import "../styles/globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,22 +24,16 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
+{/*                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
-                  </div>
+                  </div> */}
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
@@ -65,7 +57,6 @@ export default function RootLayout({
               </footer>
             </div>
           </main>
-        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import Hero from "@/src/components/hero";
+import ConnectSupabaseSteps from "@/src/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/src/components/tutorial/sign-up-user-steps";
+import GroupPage from "@/src/components/pages/GroupPage/GroupPage";
+import GoalCard from "@/src/components/molecules/GoalCard/GoalCard";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      {/* <Hero /> */}
+      <GroupPage />
+{/*       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+        {hasEnvVars ? <ActivitiesList /> : <ConnectSupabaseSteps />}
+      </main> */}
     </>
   );
 }

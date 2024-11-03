14084383df1a57d9681c70adc2e59d14ec36809f
Rafael Leitao <carlosrafael093@gmail.com>
2024-11-03 15:02:59 +0100

    feat(form-message): Color msgs based on type

--- src/components/form-message.tsx
+++ src/components/form-message.tsx
@@ -4,20 +4,23 @@ export type Message =
   | { message: string };
 
 export function FormMessage({ message }: { message: Message }) {
+  const baseStyle = 'text-foreground border-l-2 border-foreground p-4 rounded-sm mb-4';
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className={`${baseStyle} border-green-600 bg-green-200`}>
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className={`${baseStyle} border-red-600 bg-red-200`}>
           {message.error}
         </div>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className={`${baseStyle} border-blue-600 bg-blue-200`}>
+          {message.message}
+        </div>
       )}
     </div>
   );
